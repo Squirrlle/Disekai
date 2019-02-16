cb56889a10596370f2506a73d969a26559e1dcea
hjcad <hjcadoff@gmail.com>
2019-02-15 20:23:59 -0500

    More template code

--- truck.js
+++ truck.js
@@ -8,8 +8,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-	if(msg.content === 'why') {
-		msg.reply("Why are we still here? Just to suffer? Every night, I can feel my leg… and my arm… even my fingers. The body I’ve lost… the comrades I’ve lost… won’t stop hurting… It’s like they’re all still there. You feel it, too, don’t you?");
+	if(msg.content === 'Hi') {
+		msg.channel.send("String");
 	}
 });
 
